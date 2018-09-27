54a8bab41eb2d729541730dbbe58af572c13a7be
TerryGlen <32306780+TerryGlen@users.noreply.github.com>
2018-09-27 11:12:36 -0500

    Update Earthquake ArrayList

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -34,12 +34,12 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Create a fake list of earthquake locations.
         ArrayList<Earthquake> earthquakes = new ArrayList<>();
         earthquakes.add(7.2, "San Francisco", "Feb 2, 2016");
-        earthquakes.add("6.1" , "London", "July 20, 2015");
-        earthquakes.add("3.9", "Tokyo", "Nov 10, 2014");
-        earthquakes.add("5.4", "Mexico City", "May 3, 2014");
-        earthquakes.add("2.8", "Moscow" , "Jan 31, 2018");
-        earthquakes.add("4.9", "Rio de Janeiro", "Aug 19,2010");
-        earthquakes.add("1.6", "Paris", "Oct 30, 2011");
+        earthquakes.add(6.1 , "London", "July 20, 2015");
+        earthquakes.add(3.9, "Tokyo", "Nov 10, 2014");
+        earthquakes.add(5.4, "Mexico City", "May 3, 2014");
+        earthquakes.add(2.8, "Moscow" , "Jan 31, 2018");
+        earthquakes.add(4.9, "Rio de Janeiro", "Aug 19,2010");
+        earthquakes.add(1.6, "Paris", "Oct 30, 2011");
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
