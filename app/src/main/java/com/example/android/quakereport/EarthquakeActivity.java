2760d43ec5ff89ffb8ba147ba566706aa80e7a28
Treecko99 <saispas99@gmail.com>
2018-10-04 08:56:43 -0500

    Updated adapter to use ArrayList<Earthquake>

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -33,13 +33,13 @@ public class EarthquakeActivity extends AppCompatActivity {
 
         // Create a fake list of earthquake locations.
         ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(7.2, "San Francisco", "Feb 2, 2016");
-        earthquakes.add(6.1 , "London", "July 20, 2015");
-        earthquakes.add(3.9, "Tokyo", "Nov 10, 2014");
-        earthquakes.add(5.4, "Mexico City", "May 3, 2014");
-        earthquakes.add(2.8, "Moscow" , "Jan 31, 2018");
-        earthquakes.add(4.9, "Rio de Janeiro", "Aug 19,2010");
-        earthquakes.add(1.6, "Paris", "Oct 30, 2011");
+        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2, 2016"));
+        earthquakes.add(new Earthquake(6.1 , "London", "July 20, 2015"));
+        earthquakes.add(new Earthquake(3.9, "Tokyo", "Nov 10, 2014"));
+        earthquakes.add(new Earthquake(5.4, "Mexico City", "May 3, 2014"));
+        earthquakes.add(new Earthquake(2.8, "Moscow" , "Jan 31, 2018"));
+        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", "Aug 19,2010"));
+        earthquakes.add(new Earthquake(1.6, "Paris", "Oct 30, 2011"));
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
